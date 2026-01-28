84eb4c3a9cbcf32f3398fa17469ca6a2f75a4880
Javier Castillo <javier.developer.kt@gmail.com>
2026-01-27 22:08:35 -0600

    Fix: corrección de lista dinámica A-D y configuración de Toolbar

--- app/src/main/java/com/javiercast/alacartacdmx/MainActivity.java
+++ app/src/main/java/com/javiercast/alacartacdmx/MainActivity.java
@@ -1,8 +1,15 @@
 package com.javiercast.alacartacdmx;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.bar);
+        setSupportActionBar(toolbar);
+
+        ListView listView = findViewById(R.id.listView);
+
+        char inicio = 'A';
+        String[] items = new String[4];
+        for (int i = 0; i < items.length; i++) {
+            char letraActual = (char) (inicio + i);
+            items[i] = "Restaurante " + letraActual;
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.app_bar_search) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
