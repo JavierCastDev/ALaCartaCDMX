487271ec1e4c414ed39f23996bcb7f4b6a9ff61a
Javier Castillo <javier.developer.kt@gmail.com>
2026-01-29 22:10:18 -0600

    Fix: Hacer funcional la SearchView y corregir ClassCastException

--- app/src/main/java/com/javiercast/alacartacdmx/MainActivity.java
+++ app/src/main/java/com/javiercast/alacartacdmx/MainActivity.java
@@ -9,9 +9,12 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,23 +33,35 @@ public class MainActivity extends AppCompatActivity {
             items[i] = "Restaurante " + letraActual;
         }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
-
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         listView.setAdapter(adapter);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.app_bar_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (adapter != null) {
+                    adapter.getFilter().filter(newText);
+                }
+                return false;
+            }
+        });
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.app_bar_search) {
-            return true;
-        }
         return super.onOptionsItemSelected(item);
     }
 }
